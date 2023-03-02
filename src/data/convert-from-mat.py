bbaf8767634afb1b4b835daee2c7b5c8ebc0fc42
Sander Løtuft <sanderlot00@gmail.com>
2023-03-02 02:20:09 +0100

    gitignore

--- src/data/convert-from-mat.py
+++ src/data/convert-from-mat.py
@@ -6,10 +6,6 @@ import os
 dir = '././data/raw/PLDM/test_gt/'
 outDir = './data/interim/PLDM/labeled/'
 
-# fig = plt.figure(frameon=False, dpi=100, figsize=(3.6, 5.4))
-# fig.figimage(sub_array5, cmap='binary', xo=0, yo=0)
-# plt.savefig('testfile.png')
-
 def convert_data(indir = '', outdir = '', key = ''):
     for file in os.listdir(indir):
         if file != '.DS_Store':
@@ -21,8 +17,3 @@ def convert_data(indir = '', outdir = '', key = ''):
             fig = plt.figure(frameon=False, dpi=100, figsize=(3.6, 5.4))
             fig.figimage(arr, cmap='binary', xo=0, yo=0)
             plt.savefig(outDir + file.removesuffix('.mat') + '.jpg')
-
-convert_data(dir, outDir, 'groundTruth')
-# Not finished yet
-# TODO: Save the plot to file in interim folder with a fixed size of 540x360 or 360x540
-# Look into saving in a single fixed format to avoid problems down the line. Ask TA/Teacher
